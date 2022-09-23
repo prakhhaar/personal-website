18324ec616adc08f378d931106e984374b567705
Prakhar Sharma <prakhar.shrm202@gmail.com>
2022-09-23 18:57:59 +0530

    added shadow after scroll

---
+++ app.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll',(e)=>{
+    const nav = document.querySelector('.nav');
+    if(window.pageYOffset>0){
+      nav.classList.add("add-shadow");
+    }else{
+      nav.classList.remove("add-shadow");
+    }
+  });

1ef4438a98f1a35d4323e6e738fc479133afa08e
Prakhar Sharma <prakhar.shrm202@gmail.com>
2022-10-05 16:28:19 +0530

    added on scroll for nav bar

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ window.addEventListener('scroll',(e)=>{
       nav.classList.remove("add-shadow");
     }
   });
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
